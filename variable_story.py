a3ae7187c2f75d91debd14126c0d5a7f51c448c2
quhao0994 <quhao0994@gmail.com>
2018-11-30 15:34:00 +0800

    Add files via upload
    variable

---
+++ variable_story.py
@@ -0,0 +1,8 @@
+character_name = "John"
+character_age = "20"
+print("There's a guy named "+character_name+".")
+print("His age is "+character_age)
+print("Mike says the beginners only need 3 kinds of data type")
+print("Now I'm gonna update a data type")
+character_age = "50"
+print("I think the character's age is probably "+character_age+", cuz I updated it.")
