f8057acbb82a03e3a2de89cf5ee85525a5ab7a3a
quhao0994 <quhao0994@gmail.com>
2018-12-01 18:21:02 +0800

    Add files via upload

---
+++ basic_calculator.py
@@ -0,0 +1,8 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = num1 + num2
+
+print(result)#得 字符串＋字符串
+
+result2 = float(num1) + float(num2)
+print(result2)#得两数之和
