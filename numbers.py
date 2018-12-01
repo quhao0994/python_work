3911c1b1a44486eeb02308cee3ab4936601b5075
quhao0994 <quhao0994@gmail.com>
2018-12-01 18:04:25 +0800

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,15 @@
+print(2*8/2+3.1)
+print(10 % 3)#modulates 取余
+my_num = 27
+print(str(my_num)+", remember make the number a string alongside strings")
+
+my_num = -100
+print(abs(my_num))#absolute number
+print(pow(4,2))#4^2=16
+print(max(1,5,3,7,9,99))#99 同理min()找最小值
+print(round(3.54))#取整4，四舍五入 round a number
+
+from math  import * #调用数学函数
+print(floor(3.7))#3 向下取整
+print(ceil(3.7))#4 向上取整
+print(sqrt(16))#4 开平方
