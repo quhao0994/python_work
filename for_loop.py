68232c252b418fedf9683fdf9aa516cded6dc8d7
quhao0994 <quhao0994@gmail.com>
2018-12-05 17:46:42 +0800

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+friends = ["jim","karen","kevin"]
+
+for name in friends:
+    print(name)
+
+
+
+for index in range(3,10):
+    print(index)#print out numbers between 3 and 10,but not including 10
+
+
+for index in range(5):
+    if index == 0:
+        print("first iteration")
+    else:
+        print("not first")
