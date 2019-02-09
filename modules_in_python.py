d3447f2a0e604664bea03d396d53e8ab0c3c8c6d
quhao0994 <quhao0994@gmail.com>
2019-02-10 00:43:45 +0800

    Add files via upload

---
+++ modules_in_python.py
@@ -0,0 +1,6 @@
+import useful_tools
+# "useful_tools" - file name
+print(useful_tools.roll_dice(10))
+# roll.dice()该文件里的function
+
+# using pip
---
+++ reading_files.py
@@ -0,0 +1,4 @@
+python_file = open("try_except.py","r")
+# reading from the file
+print(python_file.read())
+python_file.close()
